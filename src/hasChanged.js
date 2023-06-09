ef193af8ac515115f949dded9b1d259392bb0d48
jesus-castro3 <jesus.castro3@gmail.com>
2023-06-09 15:29:35 -0700

    trial and error

--- src/hasChanged.js
+++ src/hasChanged.js
@@ -20,13 +20,19 @@ async function hasChanged(pathsToSearch) {
   const paths = pathsToSearch.split(" ");
 
   //  --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
+
+  await exec.exec("git", ["fetch", "origin", "master"], {
+    ignoreReturnCode: true,
+    silent: false,
+    cwd: getCWD(),
+  });
   const exitCode = await exec.exec(
     "git",
-    ["diff", "origin/master...HEAD", "--", ...paths],
+    ["diff", "--quiet", "FETCH_HEAD...HEAD", "--", ...paths],
     {
       ignoreReturnCode: true,
       silent: false,
-      // cwd: getCWD(),
+      cwd: getCWD(),
     }
   );
 
